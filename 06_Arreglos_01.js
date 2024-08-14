687d527030c38dc2420f50ed2a7bf5f5c23aae30
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-08-14 18:59:49 -0500

    Update and rename 06_Arreglos_01 to 06_Arreglos_01.js

---
+++ 06_Arreglos_01.js
@@ -0,0 +1,26 @@
+
+/*### **Ejercicio**
+
+Crea una función llamada `primerElemento` que reciba un arreglo y retorne el primer elemento del arreglo.
+
+Ejemplo: primerElemento(["Juan", "Pedro", "Ana", "Luis"]) // Juan
+primerElemento([1, 2, 3, 4, 5]) // 1*/
+
+
+/*Casos de prueba
+Puedes revisar los argumentos que ejecutaremos para evaluar tu función:
+Test 1	[3,4,1,5]
+Test 2	["e","t","r"]
+Test 3	["ruby", "javascript", "python"]
+*/
+
+function primerElemento (arreglo){
+return arreglo[0];
+}
+
+//resultado 
+/*
+3
+e
+ruby
+*/
