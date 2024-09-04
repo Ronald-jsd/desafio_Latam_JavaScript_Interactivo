2b2e7fda4cfbc0f6a05b7a2efd95c16f16c9aff1
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 18:33:07 -0500

    Create 06_Ciclos_01.js

---
+++ 06_Ciclos_01.js
@@ -0,0 +1,20 @@
+/*
+Crea una función llamada contarHasta que reciba un número como parámetro
+y que muestre en consola los números del 1 al número ingresado.
+*/
+function contarHasta(num){
+    let i = 1
+  while( i <=num){
+    console.log(i);
+    i++;
+    
+  }
+}
+
+contarHasta(4)
+contarHasta(12)
+/*
+Salida
+1 2 3 4
+1 2 3 4 5 6 7 8 9 10 11 12
+*/
