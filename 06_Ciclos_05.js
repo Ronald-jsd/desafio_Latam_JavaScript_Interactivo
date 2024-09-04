b90ec5200a26a0482552df0325e31c7478990475
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 18:42:26 -0500

    Create 06_Ciclos_05.js

---
+++ 06_Ciclos_05.js
@@ -0,0 +1,24 @@
+/*
+### **Ejercicio**
+
+El siguiente código está incompleto y por lo mismo produce un 
+ciclo infinito. Complétalo para que muestre los números del 4 al 8.
+
+contador = 4
+while (contador <= 8) 
+  console.log(contador)
+  contador = contador + 1
+
+*/
+
+contador = 4
+while (contador <= 8) {
+  console.log(contador)
+  contador = contador + 1
+}
+
+
+/*
+Salida:
+4 5 6 7 8 
+*/
