01bdfdbebb89f35902bda2c012b9097f9087e7dc
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 18:56:49 -0500

    Create 06_Ciclos_10.js

---
+++ 06_Ciclos_10.js
@@ -0,0 +1,31 @@
+/*
+Ejercicio
+Crea una función llamada introduccionAFor que muestre en consola los 
+números del 1 al 15 utilizando la instrucción for.
+*/
+function introduccionAFor(){
+    for(let i = 1 ; i <= 15; i++){
+        console.log(i);
+    }
+}
+
+introduccionAFor()
+
+/*
+Salida:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+*/
