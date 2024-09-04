9908c1329ea14605a457201b3e3e2e4f7206d1d1
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 18:37:25 -0500

    Create 06_Ciclos_04.js

---
+++ 06_Ciclos_04.js
@@ -0,0 +1,28 @@
+/*
+### **Ejercicio**
+
+El siguiente código está incompleto y por lo mismo produce un ciclo infinito. 
+Ejecútalo para que veas el error.
+
+Luego, complétalo para que muestre en consola los números del 1 al 10.
+contador = 1
+while (contador <= 10) {
+    console.log(contador) 
+
+}
+
+*/
+
+contador = 1
+
+while (contador <= 10) {
+contador++; //respuesta
+    console.log(contador) 
+}
+
+
+/*
+Salida:
+1 2 3 4 5 6 7 8 9 10
+
+*/
