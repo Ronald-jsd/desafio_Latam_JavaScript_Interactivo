46b4a078b24a0ab748cc99ae8d88f124af909927
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 19:10:51 -0500

    Create Create 06_Ciclos_13.js

---
+++ Create 06_Ciclos_13.js
@@ -0,0 +1,20 @@
+/*
+Ejercicio
+Utilizando la estructura de un ciclo for, muestra los números del 10 al 1.
+*/
+for(let i = 10;i>0;i--){
+    console.log(i);
+}
+
+/*
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
