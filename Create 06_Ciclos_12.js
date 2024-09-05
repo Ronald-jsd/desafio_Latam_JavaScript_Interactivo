5e5aaa4a7784c1015c3bc2a1ca43a00d886f2b21
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 19:06:26 -0500

    Create Create 06_Ciclos_12.js

---
+++ Create 06_Ciclos_12.js
@@ -0,0 +1,21 @@
+/*Ejercicio
+Modificando sólo la línea de instrucción de for,
+cambia el valor de la variable contador para que
+muestre en consola los números impares del 1 al 10
+
+for (contador = 1; contador <= 10; contador = contador + 1) {
+    console.log(contador)
+}*/
+
+for (contador = 1; contador <= 10; contador = contador + 2) {
+    console.log(contador)
+}
+
+/*
+Salida:
+1
+3
+5
+7
+9
+*/
