5c18bbfc89dba7d5d833d48f7da0f99b7c71be45
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2024-09-04 19:02:21 -0500

    Create Create 06_Ciclos_11.js

---
+++ Create 06_Ciclos_11.js
@@ -0,0 +1,31 @@
+/*
+Ejercicio
+Crea la función mostrarNumeros que reciba dos parámetro, inicio y fin.
+
+La función debe mostrar en consola todos los números desde el inicio hasta el fin.
+*/
+
+function mostrarNumeros(inicio, fin){
+    for(let i = inicio; i <= fin; i++){
+        console.log(i);
+    }
+}
+
+mostrarNumeros(4,9)
+mostrarNumeros(20,25)
+
+/*
+4
+5
+6
+7
+8
+9
+
+
+20
+21
+22
+23
+24
+25*/
